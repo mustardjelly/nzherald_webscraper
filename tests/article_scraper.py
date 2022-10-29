82fd535eef56afc8d8325a81df71bb9cbc284432
The Sam <muer.powell@gmail.com>
2022-10-29 20:58:18 +1300

    Added tests

--- tests/article_scraper.py
+++ tests/article_scraper.py
@@ -1,7 +1,8 @@
+from decimal import DivisionByZero
 import logging
 import os
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock, patch
 
 from bs4 import BeautifulSoup
 
@@ -11,7 +12,7 @@ from assets.article_scraper import Article
 logging.basicConfig(level=logging.DEBUG)
 
 
-class ArticleScaperTests(unittest.TestCase):
+class ArticleScaper(unittest.TestCase):
 
     def setUp(self) -> None:
         logging.info(f"Setting Up - {self._testMethodName}")
@@ -21,15 +22,8 @@ class ArticleScaperTests(unittest.TestCase):
         return super().setUp()
 
     def tearDown(self) -> None:
-        # Destroy or clean up after running tests
         logging.info("Tearing Down")
         return super().tearDown()
-
-    def test_scrape_title(self):
-        """Scrape the title from our test asset and assert that its working as intended"""
-        result = self.scraper.scrape_title()
-        expected = "Gregor paul: women showing the men how it's done at the rugby world cup"
-        self.assertEqual(result, expected)
 
     def test_init_loads_files(self):
         """
@@ -59,8 +53,64 @@ class ArticleScaperTests(unittest.TestCase):
 
             self.assertEqual(actual_error_msg, expected_error_msg)
 
+    def test_scrape_title(self):
+        """Scrape the title from our test asset and assert that its working as intended"""
+        result = self.scraper.scrape_title()
+        expected = "Gregor paul: women showing the men how it's done at the rugby world cup"
+        self.assertEqual(result, expected)
 
+    def test_mocking(self):
+        mock = MagicMock(return_value="Ran!")
+        self.scraper.run = mock
+        self.scraper.run("1")
+        self.scraper.run("2")
+        self.scraper.run("3")
 
-    # def test_load_file(self):
-    #   pass
+        self.assertEqual(mock.call_count, 3)
+
+    def test_mocking2(self):
+        mock = Mock(side_effect=DivisionByZero('foo'))
+        self.scraper.run = mock
+        with self.assertRaises(DivisionByZero):
+            self.scraper.run("1")
+        
+    def test_scrape_image(self):
+        # Arrange
+        self.scraper.scrape_title()
+        self.scraper.scrape_text()
+
+        # Act
+        self.scraper.scrape_image()
+
+        # Assert
+        self.assertIsNotNone(self.scraper.article_image)
+        # Check the contents of article_image
+        
+    def test_scrape_image_if_no_1440_does_nothing(self):
+        """
+        If no 1440 found in img tags, returns nothing
+        """
+        # Arrange
+        self.scraper.scrape_title()
+        self.scraper.scrape_text()
+        self.scraper._html.find_all = MagicMock(return_value=["1440"])
+
+        # Act
+        self.scraper.scrape_image()
+
+        # Assert
+        self.assertIsNone(self.scraper.article_image)
+        # Check the contents of article_image
+
+    def test_scrape_image_called_out_of_order(self):
+        """
+        Test calling scrape image out of sequence raises an error.
+        """
+        with self.assertRaises(TypeError) as cntxt:
+            self.scraper.scrape_image()
+
+            actual_error = str(cntxt.exception)
+            expected_error = "expected string or bytes-like object"
+            self.assertEqual(actual_error, expected_error)
+            self.assertIsNone(self.scraper._article_title)
 
